44e4f0fdaee3d930aecad4c98eed279d59224fb5
Luna Heyman <luna.heyman@proton.me>
2024-01-05 13:14:45 +0100

    refactor: move basic configuration into a module

--- hosts/luna-desktop/configuration.nix
+++ hosts/luna-desktop/configuration.nix
@@ -1,67 +1,16 @@
 { pkgs, config, ... }:
 {
-  imports = [ ./hardware-configuration.nix ];
+  imports = [ ./hardware-configuration.nix ../../modules/nixos ];
+
+  enable-nvidia = true;
 
   system.stateVersion = "23.11";
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nixpkgs.config.allowUnfree = true;
-
   networking.hostName = "luna-desktop";
   time.timeZone = "Europe/Brussels";
   i18n.defaultLocale = "en_US.UTF-8";
-
-  console.useXkbConfig = true;
-
-  sound.enable = true;
-
-  security.sudo.extraConfig = "Defaults env_reset,pwfeedback";
-
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
-  };
-
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-    open = false;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
-  hardware.pulseaudio = {
-    enable = true;
-    support32Bit = true;
-  };
 
   users.users.luna = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
   };
-
-  services.xserver = {
-    enable = true;
-    videoDrivers = [ "nvidia" ];
-    libinput.enable = true;
-    displayManager.startx.enable = true;
-
-    xkb = {
-      layout = "us";
-      options = "caps:escape";
-    };
-
-    windowManager.xmonad = {
-      enable = true;
-      enableContribAndExtras = true;
-    };
-  };
-
-  fonts.packages = with pkgs; [
-    work-sans
-    (nerdfonts.override { fonts = [ "FiraCode" ]; })
-  ];
 }
---
+++ modules/nixos/default.nix
@@ -0,0 +1,65 @@
+{ pkgs, config, lib, ... }:
+let
+  inherit (lib) mkIf mkOption types;
+in
+{
+  options = {
+    enable-nvidia = mkOption {
+      type = types.bool;
+      default = false;
+    };
+  };
+
+  config = {
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    nixpkgs.config.allowUnfree = true;
+
+    console.useXkbConfig = true;
+    sound.enable = true;
+    security.sudo.extraConfig = "Defaults env_reset,pwfeedback";
+
+    boot.loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+
+    hardware.opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+
+    hardware.nvidia = mkIf config.enable-nvidia {
+      modesetting.enable = true;
+      open = false;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+
+    hardware.pulseaudio = {
+      enable = true;
+      support32Bit = true;
+    };
+
+    services.xserver = {
+      enable = true;
+      videoDrivers = mkIf config.enable-nvidia [ "nvidia" ];
+      libinput.enable = true;
+      displayManager.startx.enable = true;
+
+      xkb = {
+        layout = "us";
+        options = "caps:escape";
+      };
+
+      windowManager.xmonad = {
+        enable = true;
+        enableContribAndExtras = true;
+      };
+    };
+
+    fonts.packages = with pkgs; [
+      work-sans
+      (nerdfonts.override { fonts = [ "FiraCode" ]; })
+    ];
+  };
+}
